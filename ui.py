3661ea6011960231e80f40a5f2e03763a3f5eed4
594b4b <yadhukk12345@gmail.com>
2022-10-24 17:01:08 +0530

    reformatted code

--- ui.py
+++ ui.py
@@ -2,19 +2,21 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from tk_functions import CreateToolTip
-import tkinterDnD
+from tkinter import messagebox
 from functions import *
+import tkinterDnD
 
 colours=["#2A2F32","#0D8ABF","#02C39A"]
 
 window=tkinterDnD.Tk()
 window.geometry("600x600")
-window.title("E-FILING HELPER")
+window.title("HCK E-FILING HELPER")
 window.configure(bg=colours[0])
 window.resizable(height=0, width=0)
 window.iconbitmap("assets/app_icon.ico")
 
-icons=[PhotoImage(file='assets/settings.png'),PhotoImage(file='assets/help.png'),PhotoImage(file='assets/csv.png'),PhotoImage(file='assets/excel.png'),PhotoImage(file='assets/mail.png'),PhotoImage(file='assets/DnD.png'),PhotoImage(file='assets/next.png'),PhotoImage(file='assets/retry.png'),PhotoImage(file='assets/dbs.png')]
+icons=[PhotoImage(file='assets/settings.png'),PhotoImage(file='assets/help.png'),PhotoImage(file='assets/mail.png'),PhotoImage(file='assets/DnD.png'),PhotoImage(file='assets/next.png')]
+# 0-settings 1-help 2-mail 3-DnD 4-next
 
 def button_pressed(id):
     if id == 0: # mail button  
@@ -22,18 +24,19 @@ def button_pressed(id):
         mail_ui()
     elif id == 1: # help button
         UI_destroy()
-        help_ui()
+        helpUI()
     elif id == 2: # settings button
         UI_destroy()
         settings_ui()
     elif id == 3: # next button
         try:
             UI_destroy()
+            pageNumberUI()
         except:
             choose_path_label.place(x=200, y=350)
             window.after(3000, choose_path_label.place_forget)
     elif id == 4: # choose file button
-        window.filename = filedialog.askopenfilename(initialdir="Documents", title="Select CSV file", filetypes=[("csv files","*.csv")])
+        window.filename = filedialog.askopenfilename(initialdir="Documents", title="Select Word file", filetypes=[("word file","*.doc"),("word file","*.docx")])
         listb.insert("end", window.filename)
     elif id == 5: # clear button
         selection = listb.curselection()
@@ -73,44 +76,33 @@ def UI_build():
     DnD_label.place(x=250, y=300)
 
 def callback():
-    # if messagebox.askokcancel("Quit", "Quit the software?"):
-    window.destroy()
-    raise SystemExit
+    if messagebox.askokcancel("Quit", "Do you want to quit the app?"):
+        window.destroy()
+        raise SystemExit
 
 listb=tk.Listbox(window, selectmode=tk.SINGLE, bg=colours[0], fg=colours[2], font=("Roboto" , 10, 'bold'), height=23, width=70, borderwidth=5)
 
-mail_button=tk.Button(window,command=lambda: button_pressed(0), image=icons[4], borderwidth =0, bg=colours[0], activebackground=colours[2], height=50, width=50)
+mail_button=tk.Button(window,command=lambda: button_pressed(0), image=icons[2], borderwidth =0, bg=colours[0], activebackground=colours[2], height=50, width=50)
 help_button=tk.Button(window,command=lambda: button_pressed(1), image=icons[1], borderwidth =0, bg=colours[0], activebackground=colours[2], height=48, width=48)
 settings_button=tk.Button(window,command=lambda: button_pressed(2),image=icons[0], borderwidth = 0, bg=colours[0], activebackground=colours[2], height=64, width=64)
-next_button=tk.Button(window,command=lambda: button_pressed(3), image=icons[6], borderwidth =0 , bg=colours[0], activebackground=colours[2], height=48, width=48)
+next_button=tk.Button(window,command=lambda: button_pressed(3), image=icons[4], borderwidth =0 , bg=colours[0], activebackground=colours[2], height=48, width=48)
 choose_file_button=tk.Button(window,command=lambda: button_pressed(4), text="CHOOSE FILE",font=("Roboto" , 10, 'bold') , bg=colours[1], activebackground=colours[2])
 clear_button=tk.Button(window,command=lambda: button_pressed(5), text="CLEAR",font=("Roboto" , 10, 'bold') , bg=colours[1], activebackground=colours[2])
 clear_all_button=tk.Button(window,command=lambda: button_pressed(6), text="CLEAR ALL",font=("Roboto" , 10, 'bold') , bg=colours[1], activebackground=colours[2])
 
-DnD_icon=tk.Label(window, image=icons[5], bg=colours[0])
+DnD_icon=tk.Label(window, image=icons[3], bg=colours[0])
 DnD_label=tk.Label(window, text="DROP THE WORD FILE",font=("Roboto" , 8, 'bold'), bg=colours[0], fg=colours[1])
 choose_path_label=tk.Label(window, text="PLEASE SELECT A FILE!",font=("Roboto" , 12, 'bold'), bg=colours[0], fg=colours[1])
 
-
-
-# fix code!!!
-
 DnD_icon.register_drop_target("*")
 DnD_icon.bind("<<Drop>>", dnd_listbox)
-
 DnD_icon.register_drag_source("*")
 DnD_icon.bind("<<DragInitCmd>>", dnd_listbox)
 
 listb.register_drop_target("*")
 listb.bind("<<Drop>>", dnd_listbox)
-
 listb.register_drag_source("*")
 listb.bind("<<DragInitCmd>>", dnd_listbox)
-
-# DnD_icon.register_drag_source("*")
-# listb.register_drag_source("*")
-# DnD_icon.bind("<<Drop>>", dnd_listbox)
-# listb.bind("<<Drop>>", dnd_listbox)
 
 CreateToolTip(mail_button, text="Mail report")
 CreateToolTip(help_button, text="Help")
@@ -122,7 +114,20 @@ CreateToolTip(clear_all_button, text="Clear All")
 
 window.protocol("WM_DELETE_WINDOW", callback)
 
-def help_ui():
+def pageNumberUI():
+    def pageNumberUIDestroy():
+        back_button.place_forget()
+        
+    def back():
+        pageNumberUIDestroy()
+        UI_build()
+
+    back_button=tk.Button(window,command= back, text="Back", font=("Roboto" , 13, 'bold') , bg=colours[1], activebackground=colours[2])
+    back_button.place(x="14",y="14")
+
+    CreateToolTip(back_button, text="Back to Main Menu")
+
+def helpUI():
     def help_destroy():
         back_button.place_forget()
         
@@ -164,7 +169,6 @@ def settings_ui():
         window.filename = filedialog.askdirectory(initialdir="Documents", title="Select Export Path")
         path=window.filename
         path_label.config(text="EXPORT PATH IS: \n"+path)
-        write_to_bin(1,path, None, None)
 
     def mail_creds_input():
         
@@ -173,7 +177,6 @@ def settings_ui():
             password=pass_input.get()
             id_input.delete(0, 'end')
             pass_input.delete(0, 'end')
-            write_to_bin(0,None,mail_id, password)
             id_label.place_forget()
             id_input.place_forget()
             pass_label.place_forget()
@@ -354,4 +357,3 @@ def UI():
 
 if __name__ == "__main__":
     UI()
-
