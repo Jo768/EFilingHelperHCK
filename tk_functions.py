b29fc7b19f3b83973ef52d5fd576203ba461a801
594b4b <yadhukk12345@gmail.com>
2022-10-26 23:43:33 +0530

    reformatted code

--- tk_functions.py
+++ tk_functions.py
@@ -1,15 +1,6 @@
-try:
-    # for Python2
-    import tkinter as tk
-except ImportError:
-    # for Python3
-    import tkinter as tk
-
+import tkinter as tk
 
 class CreateToolTip(object):
-    '''
-    create a tooltip for a given widget
-    '''
     def __init__(self, widget, text='widget info'):
         self.widget = widget
         self.text = text
@@ -21,9 +12,7 @@ class CreateToolTip(object):
         x, y, cx, cy = self.widget.bbox("insert")
         x += self.widget.winfo_rootx() + 25
         y += self.widget.winfo_rooty() + 20
-        # creates a toplevel window
         self.tw = tk.Toplevel(self.widget)
-        # Leaves only the label and removes the app window
         self.tw.wm_overrideredirect(True)
         self.tw.wm_geometry("+%d+%d" % (x, y))
         label = tk.Label(self.tw, text=self.text, justify='left',
@@ -35,16 +24,5 @@ class CreateToolTip(object):
         if self.tw:
             self.tw.destroy()
 
-# testing ...
 if __name__ == '__main__':
-    root = tk.Tk()
-
-    btn1 = tk.Button(root, text="button 1")
-    btn1.pack(padx=10, pady=5)
-    button1_ttp = CreateToolTip(btn1, "mouse is over button 1")
-
-    btn2 = tk.Button(root, text="button 2")
-    btn2.pack(padx=10, pady=5)
-    button2_ttp = CreateToolTip(btn2, "mouse is over button 2")
-
-    root.mainloop()
+    pass
